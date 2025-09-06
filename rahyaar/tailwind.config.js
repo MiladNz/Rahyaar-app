d36ea25272045e0d5db8838c55685da1f903ea03
miladn <milad.n91@gmail.com>
2025-09-06 14:46:02 +0330

    modify some bug

---
+++ rahyaar/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2835A7",
+        secondary: "#D8E7FF",
+      },
+    },
+  },
+  plugins: [],
+};
