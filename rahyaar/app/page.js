0b927bbfa956d0bb5e12b504b4fef5c0800eb824
miladn <milad.n91@gmail.com>
2025-10-15 15:55:43 +0330

    remove some files

--- rahyaar/app/page.js
+++ rahyaar/app/page.js
@@ -1,9 +1,7 @@
 import Hero from "@/components/sections/Hero";
-import { getToursAction } from "./actions/getTours";
 import SearchSection from "@/components/search/SearchSection";
 
 export default async function Home() {
-  const initialTours = await getToursAction();
   return (
     <>
       <Hero />
