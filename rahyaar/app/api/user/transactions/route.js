2640997d38682d7281edcf4f88f75d70c82949e8
miladn <milad.n91@gmail.com>
2025-11-17 00:41:49 +0330

    create api route for user's transactions

---
+++ rahyaar/app/api/user/transactions/route.js
@@ -0,0 +1,33 @@
+import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
+
+export async function GET() {
+  try {
+    const cookieStore = await cookies();
+    const accessToken = cookieStore.get("accessToken")?.value;
+
+    if (!accessToken) {
+      return NextResponse.json({ error: "توکن یافت نشد" }, { status: 401 });
+    }
+
+    const res = await fetch("http://localhost:6500/user/transactions", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    if (!res.ok) {
+      return NextResponse.json(
+        { error: "خطا در دریافت اطلاعات کاربر" },
+        { status: res.status }
+      );
+    }
+
+    const userData = await res.json();
+    return NextResponse.json(userData);
+  } catch (error) {
+    return NextResponse.json({ error: "خطای سرور" }, { status: 500 });
+  }
+}
