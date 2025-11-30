ec8785e532b1dbd1ca28e699419538967ae439e2
miladn <milad.n91@gmail.com>
2025-11-30 22:21:18 +0330

    add rahyaar api

--- rahyaar/next.config.mjs
+++ rahyaar/next.config.mjs
@@ -1,8 +1,21 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: "http",
+  //       hostname: "localhost",
+  //       port: "6500",
+  //     },
+  //   ],
+  // },
   images: {
     remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "آدرس دامنه بک اند",
+      },
       {
         protocol: "http",
         hostname: "localhost",
